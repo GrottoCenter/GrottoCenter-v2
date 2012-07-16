72a1b1cfd30397db754d0d9cab33144dfadcd003
Benjamin Soufflet <bsoufflet@gmail.com>
2012-07-16 12:13:48 +0200

    Define the variable $bgColorHex to "ffffff" to prevent "undefined variable" php notice.

--- webapp/images/gen/getCorner.php
+++ webapp/images/gen/getCorner.php
@@ -15,13 +15,14 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with GrottoCenter.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @copyright Copyright (c) 2009-2012 Clément Ronzon
+ * @copyright Copyright (c) 2009-2012 Clï¿½ment Ronzon
  * @license http://www.gnu.org/licenses/agpl.txt
  */
 ob_start("ob_gzhandler");
 header ("Content-type: image/png");
 //Global parameters
 $fgColorHex = (isset($_GET['fgc'])) ? $_GET['fgc'] : 'ffffff';
+$bgColorHex = "ffffff";
 $width = 4;
 $height = 4;
 $bgRed = hexdec(substr($bgColorHex, 0, 2));
