777c855c5fdd0b5f68f6c1d9353f911feeb5ebbc
Urien <fredericurien@gmail.com>
2018-09-14 15:41:46 +0200

    Update upload_restrictions.php
    Add .gpx extension

--- webapp/func/upload_restrictions.php
+++ webapp/func/upload_restrictions.php
@@ -22,7 +22,7 @@
 $upload_restrictions_ext_array = array("add_avatar" => array(".png",".gif",".jpg",".jpeg"),
                                         "add_logo" => array(".png",".gif",".jpg",".jpeg"),
                                         "add_attachment" => array(".zip", ".rar", ".7z", ".tar", ".gz", ".bz2"),
-                                        "add_topo" => array(".png",".gif",".jpg",".jpeg",".tiff",".bmp",".pdf", ".svg", ".csv"));
+                                        "add_topo" => array(".png",".gif",".jpg",".jpeg",".tiff",".bmp",".pdf", ".svg", ".csv", ".gpx"));
 // Max size in octets
 $upload_restrictions_size_array = array("add_avatar" => 30000,
                                         "add_logo" => 30000,
